1592cf0a074205936005ee3b45a5dfc25bcdc1c9
danishfarooq83 <101505646+danishfarooq83@users.noreply.github.com>
2023-01-30 01:17:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ b.addEventListener("click", function (e) {
   const result = Number(a.value);
   if (result % 2 == 0) {
     alert(result + "  is even ");
-  } if else(result == 0){
-alert("error no number found");
-}
+  }
 else {
     alert(result + "  is Odd ");
   }
